f82728679b5af91dbdd19c2fdd3d1297d7c891cf
Jonathan William <Jjoninha11@hotmail.com>
2023-01-25 09:23:14 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,52 +1,66 @@
 package boardgame;
 
 public class Board {
-	
+
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
-	
-	
-	
+
 	public Board(int rows, int colums) {
+		if (rows < 1 || colums < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.columns = colums;
 		pieces = new Piece[rows][columns];
 	}
 
-
-
 	public int getRows() {
 		return rows;
 	}
-
-
-
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
-
 
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
 	public Piece pieces(int row, int columns) {
+		if (!positionExists(row, columns)) {
+			throw new BoardException("Position not one the board");
+		}
 		return pieces[row][columns];
 	}
+
 	public Piece pieces(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not one the board");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
-	
+
 	public void placePiece(Position position, Piece piece) {
-		pieces [position.getRow()][position.getColumn()] = piece;
+		if (thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on position" + position);
+		}
+		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
-	
+
 	}
-	
+
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+
+	}
+
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+
+	public boolean thereIsAPiece(Position position) {
+
+		if (!positionExists(position)) {
+			throw new BoardException("Position not one the board");
+		}
+
+		return pieces(position) != null;
+	}
 }
