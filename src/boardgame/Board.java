e018c32672d9cd56f355304459bb37346364d1e7
Jonathan William <Jjoninha11@hotmail.com>
2023-01-26 07:05:12 -0300

    ChessException and ChessPosition

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -37,7 +37,7 @@ public class Board {
 		return pieces[position.getRow()][position.getColumn()];
 	}
 
-	public void placePiece(Position position, Piece piece) {
+	public void placePiece(Piece piece, Position position) {
 		if (thereIsAPiece(position)) {
 			throw new BoardException("There is already a piece on position" + position);
 		}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,32 +1,35 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
-	
+
 	private Board board;
-	
-	
+
 	public ChessMatch() {
 		board = new Board(8, 8);
 		initialSetup();
 	}
-	
+
 	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-		 for(int i = 0; i < board.getRows(); i++) {
-			 for(int j = 0; j < board.getColumns(); j++) {
-				 mat[i][j] = (ChessPiece) board.pieces(i,j);
-			 }
-		 }
-		 return mat;
+		for (int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColumns(); j++) {
+				mat[i][j] = (ChessPiece) board.pieces(i, j);
+			}
+		}
+		return mat;
 	}
-	
+
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+
 	private void initialSetup() {
-		board.placePiece(new Position(2, 1), new Rook(board, Color.WHITE));
-		board.placePiece(new Position(0, 4), new King(board, Color.BLACK));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+
 	}
 }
